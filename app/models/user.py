a1ad24fde3031fbd3ed6e379da41511bd2b03714
corentinb <corentinb@malilart.lan>
2022-10-20 15:13:29 +0200

    fix: document id

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from typing import Dict, List, Literal, Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from bson.objectid import ObjectId
 from pydantic.networks import EmailStr
-
+from app.models.utils import PyObjectId
 
 class User(BaseModel):
+    id: Optional[PyObjectId] = Field(default_factory=PyObjectId, alias="_id")
     status: str
     surname: str
     name: str
