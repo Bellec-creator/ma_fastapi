8e0a7a0bfa2b052480157b9d3af3ceeadcb686bc
corentinb <corentinb@malilart.lan>
2022-10-20 12:54:06 +0200

    fix: rename route

--- app/routes/test.py
+++ app/routes/test.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter, Body, HTTPException
 router_test = APIRouter()
 
 @router_test.get("/test")
-async def root():
+async def hello_world():
     return {"message": "Hello World"}
 
 @router_test.get("/test2")
-async def root2(string:str):
+async def test_string(string:str):
     return {"message": string}
